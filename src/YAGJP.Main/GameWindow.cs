1ecd5ba31edd09a81510f3473bfbe2c03c25f964
Yunatan <unatan77@gmail.com>
2015-12-12 10:33:52 +0300

    initial commit

---
+++ src/YAGJP.Main/GameWindow.cs
@@ -0,0 +1,21 @@
+﻿using Otter;
+using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace YAGJP.Main
+{
+    class GameWindow : Game
+    {
+        const string windowTitle = "YAGJP";
+        const int width = 640;
+        const int height = 480;
+
+        public GameWindow() : base(windowTitle, width, height)
+        {
+            
+        }
+    }
+}
